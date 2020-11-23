d73998e58d2c61e70c7e3cbb73e9abf91926dba9
Manuel Salvador <37379416+MrManu96@users.noreply.github.com>
2020-11-23 12:53:42 -0600

    Create fcubic.m

---
+++ fcubic.m
@@ -0,0 +1,9 @@
+%%%%% Archivo fcubic.m %%%%%
+function filtro = fcubic(T) x = -2 : 1/T : 2;
+x = x(2:end-1);
+y = zeros(size(x));
+for i = 1:size(x,2) if abs(x(i)) < 1
+y(i) = 1-2*abs(x(i))^2 + abs(x(i))^3; else
+y(i) = 4 - 8*abs(x(i)) + 5*abs(x(i))^2 - abs(x(i))^3;
+end end
+filtro = y'*y; end 
